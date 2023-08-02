d4163246b2ee079723d36f68437ae3f4fd8f699d
Rahil Anwar <itsrahilanwar@gmail.com>
2023-08-03 02:43:04 +0530

    Model-5

--- drive-download-20230802T203802Z-001/script.js
+++
@@ -1,9 +0,0 @@
-const names = ["John", "Jane", "Jesse", "Jessica", "Jim"];
-
-for (const name of names) {
-  if (name.startsWith("J") || name.startsWith("j")) {
-    console.log("Goodbye J" + name);
-  } else {
-    console.log("Hello " + name);
-  }
-}
